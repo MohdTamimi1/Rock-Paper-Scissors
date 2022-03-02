10674664530f9cfdda9f279574d7b807db668ab9
Mohd Tamimi <mohd.tt1987@gmail.com>
2022-03-01 19:09:38 -0500

    playerEl created to ask player for value

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-//fucntion created to show a random value to the computer Element
 
 let computerEl = ["Rock", "Paper", "Scissors"];
+let playerEl = prompt("Enter Rock, Paper or Scissors to start the game")
+console.log(playerEl)
 
+//fucntion ComputerPlay - to show a random value to the computerEl
 function computerPlay() {
     computerEl = computerEl[Math.floor(Math.random() * computerEl.length)];;
     return computerEl;
