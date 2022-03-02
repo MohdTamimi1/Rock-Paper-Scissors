31e2e5ac3ffe05a3276865fd6bedb7ccc73a17ff
Mohd Tamimi <mohd.tt1987@gmail.com>
2022-03-01 20:13:38 -0500

    playerPlay, playRound and comments added

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 
 let computerEl = ["Rock", "Paper", "Scissors"];
-let playerEl = prompt("Enter Rock, Paper or Scissors to start the game")
-console.log(playerEl)
+let playerEl = prompt("Enter Rock, Paper or Scissors to start the game");
 
-//fucntion ComputerPlay - to show a random value to the computerEl
+//function ComputerPlay - to show a random value to the computerEl
 function computerPlay() {
-    computerEl = computerEl[Math.floor(Math.random() * computerEl.length)];;
-    return computerEl;
+    computerEl = computerEl[Math.floor(Math.random() * computerEl.length)];
+    return computerEl.toLowerCase();
 }
-computerPlay()
-console.log(computerEl)
 
+//function playerPlay - to Ask a random value to the playerEl
+function playerPlay() {
+    playerEl = playerEl.toLowerCase();
+    return playerEl;
+}
+//function playRound - to compare the valye from playerEl and the value to the computerEl
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's A Tie";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You lose! Scissors beats Paper";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You Win! Scissors beats Paper";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors";
+    }
+}
+
+
+let playerSelection = playerPlay();
+console.log(playerSelection)
+let computerSelection = computerPlay()
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection))
