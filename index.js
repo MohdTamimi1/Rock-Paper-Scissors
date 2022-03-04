33f4bf3b8f06de5230e66cc519afba70c52bedb8
Mohd Tamimi <mohd.tt1987@gmail.com>
2022-03-04 17:30:30 -0500

    Game plan with comments

---
+++ index.js
@@ -0,0 +1,20 @@
+// Global variables
+
+
+// in the first stage before building an interface the game will run in the console
+
+// game function to start the game 
+function game() {
+
+}
+
+
+// function to give the computer random choice each time we run the function
+function computerChoice() {
+
+}
+
+// function to ask the user for input
+function playerChoice() {
+
+}
