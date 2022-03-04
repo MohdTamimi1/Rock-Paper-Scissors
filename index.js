4aeeac9b901865f74a6f55f41cd8aae2167cb1dc
Mohd Tamimi <mohd.tt1987@gmail.com>
2022-03-04 15:10:13 -0500

    added score var and still testing game logic

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 let computerSelection = computerPlay();
 let playerSelection = playerPlay();
-// let gameRounds = playRound(playerSelection, computerSelection);
-// let result = [];
+let gameRounds = game();
+let computerScore = 0;
+let PlayerScore = 0;
 
 //function ComputerPlay - to show a random value to the computerEl
 
 function computerPlay(computerChoice) {
-    computerChoice = ["rock", "paper", "scissors"];
+    let computerChoice = ["rock", "paper", "scissors"];
     computerChoice = computerChoice[Math.floor(Math.random() * computerChoice.length)];
     return computerChoice.toLowerCase();
 }
@@ -37,9 +38,17 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-// console.log(playerSelection);
-// console.log(computerSelection);
-// console.log(playRound(playerSelection, computerSelection));
+
+
+// console.log(gameRounds);
 
 // game function is to play a 5 round game that keeps score and reports a winner or loser at the end
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playerPlay(), i);
+        console.log(computerPlay(), i);
+        console.log(playRound(playerSelection, computerSelection), i);
+    }
+}
 
+console.log(gameRounds)
