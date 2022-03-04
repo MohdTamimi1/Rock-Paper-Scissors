7dfa28399a5ea55ec565555abaad3bfe44ffa71a
Mohd Tamimi <mohd.tt1987@gmail.com>
2022-03-04 17:34:33 -0500

    random choice to computer function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Global variables
-
+const choices = ["rock", "paper", "scissors"];
 
 // in the first stage before building an interface the game will run in the console
 
@@ -11,7 +11,7 @@ function game() {
 
 // function to give the computer random choice each time we run the function
 function computerChoice() {
-
+    return choices[Math.floor(Math.random() * choices.length)]
 }
 
 // function to ask the user for input
